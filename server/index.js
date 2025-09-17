62da2b688a3d260f4a1c0e108b88f13688a5f693
preksha3005 <preksha.g504@gmail.com>
2025-09-17 14:32:35 +0530

    new search func

--- server/index.js
+++ server/index.js
@@ -462,7 +462,7 @@ app.post("/search", verifyuser, async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
   );
 }
 
