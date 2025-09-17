f62b5c1820c84261a04ff7766a02fe9dc9bd17b2
preksha3005 <preksha.g504@gmail.com>
2025-09-17 12:39:40 +0530

    change backend

--- server/index.js
+++ server/index.js
@@ -464,8 +464,8 @@ app.post("/search", verifyuser, async (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+  app.get("/*", (req, res) =>
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
   );
 }
 
